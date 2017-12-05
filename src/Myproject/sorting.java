aefcf07e01b6f2a2b1dd71b9454e86de2bc9265d
bill1201 <bill19981201@gmail.com>
2017-12-05 14:17:07 +0800

    sorting

---
+++ src/Myproject/sorting.java
@@ -0,0 +1,20 @@
+package Myproject;
+
+public class sorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n[] = { 51, 23, 6, 65, 15 };
+		for (int i = 0; i < n.length; i++) {
+			for (int f = i+1; f < n.length; f++) {
+				if (n[f] < n[i]) {
+					int tmp = n[f];
+					n[f] = n[i];
+					n[i] = tmp;
+				}
+			}
+			System.out.println(n[i]);
+		}
+	}
+	
+}
